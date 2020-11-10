db5efc18802005bd38bffd29f263b730330c8caf
JanneHarjamaki <janne.harjamaki@gmail.com>
2020-11-10 13:50:12 -0800

    Uusi yritys tehda perusrepo turtlelle

--- src/autoturtle/src/randomwalk_wallavoider_node.py
+++ src/autoturtle/src/randomwalk_wallavoider_node.py
@@ -28,7 +28,7 @@ def callback(data):
         # vel_msg.angular.z = random.randint(-2,2) # joku luku valilta -2..2
         # vel_msg.angular.z = random.randint(-5,5) # joku luku valilta -5..5, isompi kannosvaihtelu
         vel_msg.angular.z = random.randint(-2,5) # joku luku valilta -2..5, kaantyy varmasti vastapaivaan
-        vel_msg.linear.x = random.randint(0,2) # etenemää 0..2
+        vel_msg.linear.x = random.randint(0,2) # etenemää 0..  2
 
     velocity_publisher.publish(vel_msg) # julkaisee viestin
 
